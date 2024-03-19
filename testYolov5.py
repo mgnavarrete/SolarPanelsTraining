7fc1c3696717cbfd3d87469c8b6904e0ae88d8cb
mgnavarrete <mgnavarrete@uc.cl>
2024-03-19 11:18:50 -0300

    test models

--- testYolov5.py
+++ testYolov5.py
@@ -1,28 +1,29 @@
 import subprocess
 import os
+
 # Configura las rutas y parámetros necesarios
-yolov7_repo_path = 'yolov7' 
-weights_path = '../best.pt'  # Ruta al archivo de pesos del modelo.
+yolov5_repo_path = 'yolov5'  # Asegúrate de ajustar esta ruta al directorio de YOLOv5
+weights_path = '../oldModels/best.pt'  # Ruta al archivo de pesos del modelo.
 data_yaml_path = '../dataset/data.yaml'  # Ruta al archivo YAML de configuración de los datos.
 img_size = 640  # Tamaño de imagen utilizado durante el entrenamiento.
 iou_threshold = 0.5  # Umbral de IoU para considerar predicciones correctas.
 conf_threshold = 0.001  # Umbral de confianza para considerar detecciones.
 results_name = 'test_results'  # Nombre del directorio de resultados.
 
-
-
-os.chdir(yolov7_repo_path)
+# Cambia al directorio de YOLOv5
+os.chdir(yolov5_repo_path)
 
 # Comando para ejecutar la evaluación
 command = [
-    'python', 'test.py',
+    'python', 'val.py',  # En YOLOv5 se utiliza val.py para la evaluación
     '--weights', weights_path,
     '--data', data_yaml_path,
-    '--task', 'test',
+    '--task', 'test',  # Asegúrate de que 'test' está definido en data.yaml
     '--img', str(img_size),
     '--iou-thres', str(iou_threshold),
     '--conf-thres', str(conf_threshold),
-    '--name', results_name
+    '--name', results_name,
+    '--exist-ok'  # Permite la sobreescritura de los resultados antiguos
 ]
 
 # Ejecutar el comando
---
+++ testYolov7.py
@@ -0,0 +1,29 @@
+import subprocess
+import os
+# Configura las rutas y parámetros necesarios
+yolov7_repo_path = 'yolov7' 
+weights_path = '../Models/best.pt'  # Ruta al archivo de pesos del modelo.
+data_yaml_path = '../dataset/data.yaml'  # Ruta al archivo YAML de configuración de los datos.
+img_size = 640  # Tamaño de imagen utilizado durante el entrenamiento.
+iou_threshold = 0.5  # Umbral de IoU para considerar predicciones correctas.
+conf_threshold = 0.001  # Umbral de confianza para considerar detecciones.
+results_name = 'test_results'  # Nombre del directorio de resultados.
+
+
+
+os.chdir(yolov7_repo_path)
+
+# Comando para ejecutar la evaluación
+command = [
+    'python', 'test.py',
+    '--weights', weights_path,
+    '--data', data_yaml_path,
+    '--task', 'test',
+    '--img', str(img_size),
+    '--iou-thres', str(iou_threshold),
+    '--conf-thres', str(conf_threshold),
+    '--name', results_name
+]
+
+# Ejecutar el comando
+subprocess.run(command)
