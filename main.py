344a0edd68045e17e979c396f1ffb717aedf7787
mgnavarrete <mgnavarrete@uc.cl>
2024-03-19 12:02:51 -0300

    test models

--- main.py
+++ main.py
@@ -2,34 +2,34 @@ import os
 import subprocess
 
 # Configura tus rutas y parámetros aquí
-yolov7_repo_path = 'yolov5'  # Actualiza esto con la ruta al repositorio de YOLOv7
-data_yaml_path = '../dataset/data.yaml'  # Actualiza esto con la ruta a tu archivo YAML
-weights_path = 'yolov5.pt'  # O cualquier otro archivo de pesos inicial que quieras usar
+yolov5_repo_path = 'yolov5'  # Asegúrate de que esta sea la ruta al repositorio de YOLOv5
+data_yaml_path = '../dataset/data.yaml'  # Asegúrate de que esta sea la ruta a tu archivo YAML
 img_size = 640
 batch_size = 8
 epochs = 10
 name = 'SolarFailuresDetection'
 
-# Cambia al directorio de YOLOv7
-os.chdir(yolov7_repo_path)
+# Cambia al directorio de YOLOv5
+os.chdir(yolov5_repo_path)
 
-# Comando para entrenar YOLOv7
+# Comando para entrenar YOLOv5 sin pesos preentrenados
 train_command = [
     'python', 'train.py',
     '--batch', str(batch_size),
-    '--cfg', 'cfg/training/yolov7.yaml',  # Asegúrate de que este archivo exista y esté configurado correctamente
+    '--cfg', 'models/yolov5s.yaml',  # Ajusta según la configuración deseada
     '--epochs', str(epochs),
     '--data', data_yaml_path,
-    '--weights', weights_path,
+    '--weights', '',  # Deja esta cadena vacía para entrenar desde cero
     '--img', str(img_size),
-    '--name', name
+    '--name', name,
+    '--cache'  # Opcional: activa el caché de imágenes para entrenamientos más rápidos
 ]
 
-# Ejecutar el comando de entrenamiento y mostrar la salida directamente
+# Ejecutar el comando de entrenamiento y capturar la salida
 result = subprocess.run(train_command)
-# Ejecutar el comando
-
 
 # Verificar y manejar errores si los hay
 if result.returncode != 0:
-    print("Error en el entrenamiento:", result.returncode)
+    print("Error en el entrenamiento:", result.stderr)
+else:
+    print("Entrenamiento completado exitosamente. Salida:", result.stdout)
