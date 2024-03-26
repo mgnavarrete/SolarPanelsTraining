e8b32ecc3063e94739a985a7a104f30a8da604bb
mgnavarrete <mgnavarrete@uc.cl>
2024-03-26 09:41:27 -0300

    yolov9

--- yolov9Train.py
+++ yolov9Train.py
@@ -10,7 +10,7 @@ model = YOLO('yolov9c.yaml')
 model.info()
 
 # Train the model on the COCO8 example dataset for 100 epochs
-results = model.train(data='dataset/data.yaml', epochs=50, imgsz=640)
+results = model.train(data='data.yaml', epochs=50, imgsz=640)
 
 # Run inference with the YOLOv9c model on the 'bus.jpg' image
 results = model('path/to/bus.jpg')
